987343fe94da5ddb6a5c5f53361e0f622f6c9b9d
UOU <gaojuzeng@hotmail.com>
2025-11-21 19:39:28 +0100

    Try to fix custom domain issue when deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ export default defineConfig({
   plugins: [react()],
   // IMPORTANT: Replace 'ReceiptScannerDK' with your actual GitHub Repository name if it's different.
   // For example, if your repo is 'my-scanner', change this to '/my-scanner/'
-  base: '/ReceiptScannerDK/', 
+  // base: '/ReceiptScannerDK/', 
+  // When using a Custom Domain (e.g. receipt-scanner.com), the base must be '/'
+  base: '/', 
   build: {
     outDir: 'dist',
   }
