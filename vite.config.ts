65d7d0804fc4f321af6614d836031bb860b50d17
UOU <gaojuzeng@hotmail.com>
2025-11-21 17:32:27 +0100

    config fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dk-receipt-scanner/', // REPLACE THIS with your GitHub repo name, e.g. '/receipt-scanner/'
+  base: '/ReceiptScannerDK/', // REPLACE THIS with your GitHub repo name, e.g. '/receipt-scanner/'
 })
